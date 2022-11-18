c7099848aa0ec4c14a4fb224d091a91f9ac9ff1b
janmats <96205973+janmats@users.noreply.github.com>
2022-11-18 09:42:25 +0300

    commmmm

--- Lab2.py
+++ Lab2.py
@@ -14,7 +14,7 @@ aCylinder = Geom_CylindricalSurface(gp_Ax3(gp_XOY()), 6.0)
 aLine2d = gp_Lin2d(gp_Pnt2d(0.0, 0.0), gp_Dir2d(1.0, 1.0))
 aSegment = GCE2d_MakeSegment(aLine2d, 0.0, pi * 2.0)
 
-helix_edge = BRepBuilderAPI_MakeEdge(aSegment.Value(), aCylinder, 0.0, 12.0 * pi).Edge()
+helix_edge = BRepBuilderAPI_MakeEdge(aSegment.Value(), aCylinder, 0.0, 24.0 * pi).Edge()
 display, start_display, add_menu, add_function_to_menu = init_display()
 
 display.DisplayShape(helix_edge, update=True)
