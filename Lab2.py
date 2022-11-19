4582d0abce3cc607cd2deb10bb274f9e4acc8bd8
Olga Matsulevich <flintrok@ya.ru>
2022-11-19 22:01:08 +0300

    lab2 updated

--- Lab2.py
+++ Lab2.py
@@ -3,19 +3,35 @@
 from math import pi
 
 from OCC.Core.gp import gp_Pnt2d, gp_XOY, gp_Lin2d, gp_Ax3, gp_Dir2d
+from OCC.Core.AIS import AIS_Shape
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeEdge
 from OCC.Core.Geom import Geom_CylindricalSurface
 from OCC.Core.GCE2d import GCE2d_MakeSegment
 
 from OCC.Display.SimpleGui import init_display
 
+display, start_display, add_menu, add_function_to_menu = init_display()
+
 # First build a helix
 aCylinder = Geom_CylindricalSurface(gp_Ax3(gp_XOY()), 6.0)
 aLine2d = gp_Lin2d(gp_Pnt2d(0.0, 0.0), gp_Dir2d(1.0, 1.0))
 aSegment = GCE2d_MakeSegment(aLine2d, 0.0, pi * 2.0)
 
+<<<<<<< Updated upstream
 helix_edge = BRepBuilderAPI_MakeEdge(aSegment.Value(), aCylinder, 0.0, 24.0 * pi).Edge()
 display, start_display, add_menu, add_function_to_menu = init_display()
+=======
+helix_edge = BRepBuilderAPI_MakeEdge(aSegment.Value(), aCylinder, 0.0, 12.0 * pi).Edge()
+>>>>>>> Stashed changes
 
-display.DisplayShape(helix_edge, update=True)
+
+ais_shp = AIS_Shape(helix_edge)
+ais_shp.SetWidth(6)
+ais_shp.SetTransparency(0.10)
+
+ais_context = display.GetContext()
+ais_context.Display(ais_shp, True)
+
+display.View_Iso()
+display.FitAll()
 start_display()
