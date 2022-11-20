e03c6d43173cf5ce28e1bd66ca35cd48ae8c1ae4
Olga Matsulevich <flintrok@ya.ru>
2022-11-20 22:24:28 +0300

    Update Lab4_2.py

--- Lab4_2.py
+++ Lab4_2.py
@@ -1,1 +1,23 @@
 #Построение деталей
+
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Cut, BRepAlgoAPI_Fuse
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeCylinder
+from OCC.Core.Graphic3d import Graphic3d_NameOfMaterial, Graphic3d_NOM_COPPER, Graphic3d_MaterialAspect
+from OCC.Core.gp import gp_Pnt, gp_Ax2, gp_Dir
+from OCC.Core.AIS import AIS_Shape, AIS_Shaded
+
+from OCC.Display.SimpleGui import init_display
+
+display, start_display, add_menu, add_function_to_menu = init_display()
+
+
+
+
+aisShape = AIS_Shape(fuse)
+
+ais_context = display.GetContext()
+ais_context.Display(aisShape, True)
+
+display.View_Iso()
+display.FitAll()
+start_display()
