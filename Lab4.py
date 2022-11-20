2e56e4f125392228f7c1e3fda00c9f682c256c50
Olga Matsulevich <flintrok@ya.ru>
2022-11-20 17:04:26 +0300

    Update Lab4.py

--- Lab4.py
+++ Lab4.py
@@ -1,8 +1,7 @@
 from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Cut, BRepAlgoAPI_Fuse
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeCylinder
-from OCC.Core.Graphic3d import Graphic3d_MATERIAL_ASPECT, Graphic3d_NameOfMaterial, Graphic3d_NOM_COPPER, \
-    Graphic3d_MaterialAspect
-from OCC.Core.gp import gp_Pnt2d, gp_XOY, gp_Lin2d, gp_Ax3, gp_Dir2d, gp_Pnt, gp_Ax2, gp_Dir
+from OCC.Core.Graphic3d import Graphic3d_NameOfMaterial, Graphic3d_NOM_COPPER, Graphic3d_MaterialAspect
+from OCC.Core.gp import gp_Pnt, gp_Ax2, gp_Dir
 from OCC.Core.AIS import AIS_Shape, AIS_Shaded
 
 
