447d80990d69acbe595fca712cfe73907e082dd1
janmats <96205973+janmats@users.noreply.github.com>
2022-11-22 00:33:42 +0300

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -12,7 +12,7 @@ from OCC.Display.SimpleGui import init_display
 
 display, start_display, add_menu, add_function_to_menu = init_display()
 
-# First build a helix
+
 aCylinder = Geom_CylindricalSurface(gp_Ax3(gp_XOY()), 6.0)
 aLine2d = gp_Lin2d(gp_Pnt2d(0.0, 0.0), gp_Dir2d(1.0, 1.0))
 aSegment = GCE2d_MakeSegment(aLine2d, 0.0, pi * 2.0)
